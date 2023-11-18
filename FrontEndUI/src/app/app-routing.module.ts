4f3c0ee790731f64316d2e8336b5e51692b7a86d
Ray C <cmcmone@gmail.com>
2023-11-18 12:51:51 -0500

    initial commit

--- FrontEndUI/src/app/app-routing.module.ts
+++ FrontEndUI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LayoutComponent } from './shared/layout/layout.component';
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LayoutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ FrontEndUI/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutComponent } from '@shared/layout/layout.component';
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
